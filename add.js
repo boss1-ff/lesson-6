28fec09148188ba854786379edbe6adcbb0c8e94
boss1-ff <aryginalfreefire@gmail.com>
2026-03-13 17:24:45 +0600

    Add files via upload

---
+++ add.js
@@ -0,0 +1,37 @@
+
+
+
+
+let width = 12;
+let height = 6;
+
+let area = width * height;
+
+console.log("Rectangle area = " + area);
+
+
+
+
+
+
+let age = 18;
+
+let futureAge = age + 5;
+
+console.log("After 5 years I will be " + futureAge);
+
+
+let phonePrice = 30000;
+let money = 450000;
+
+let remaining = money - phonePrice;
+
+console.log("Money left = " + remaining);
+
+
+let distance = 120;
+let time = 2;
+
+let speed = distance / time;
+
+console.log("Average speed = " + speed + " km/h");
